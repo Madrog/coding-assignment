bcd00b275bcee945021d6c5e1560a290508406ad
Madrog <rogers.madda@gmail.com>
2020-02-15 17:51:01 +0300

    added migrations

---
+++ database/migrations/2020_02_15_141803_create_individuals_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateIndividualsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('individuals', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('sacco_id');
+            $table->text('first_name');
+            $table->text('last_name');
+            $table->text('email');
+            $table->text('gender');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('individuals');
+    }
+}
