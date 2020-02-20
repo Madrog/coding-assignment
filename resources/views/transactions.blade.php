6da73f4b5f8016a8df2f24d1c8870c69d4fd6ec0
Madrog <rogers.madda@gmail.com>
2020-02-20 09:30:07 +0300

    update File Uploader

--- resources/views/transactions.blade.php
+++ resources/views/transactions.blade.php
@@ -5,16 +5,16 @@
     <div class="title m-b-md">Transactions</div>
     <div id="app">
         <div class="row">
-            <div class="col-md-8">
+            <div class="col-md-12">
                 <simple-upload></simple-upload>
+            </div>    
+        </div>
+        <div class="row">
+            <div class="col-md-8">
+                <index-component></index-component>
             </div>
             <div class="col-md-4">
                 <summary-component></summary-component>
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-12">
-                <index-component></index-component>
             </div>
         </div>
     </div>
