93dae84a044af07b6bfcf9b358ea90496cc49d80
Madrog <rogers.madda@gmail.com>
2020-02-23 22:26:42 +0300

    updated Notification messages

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -60,23 +60,7 @@
     <body>
         @include('navbar')
         <div class="container">   
-            @if($errors->any())
-                <div class="alert alert-danger">
-                    <a href="#" class="close" data-dismiss="alert" aria-label="close">x</a>
-                    <ul>
-                        @foreach($errors->all() as $error)
-                            <li>{{ $error }}</li>
-                        @endforeach
-                    </ul>
-                </div>
-            @endif
-        
-            @if (Session::has('success'))
-                <div class="alert alert-success">
-                    <a href="#" class="close" data-dismiss="alert" aria-label="close">x</a>
-                    <p>{{ Session::get('successs') }}</p>
-                </div>
-            @endif        
+           
             @yield('content')
         </div>
     </body>
--- resources/views/transactions.blade.php
+++ resources/views/transactions.blade.php
@@ -3,6 +3,24 @@
 
 @section('content')
     <div class="title m-b-md">Transactions</div>
+    @if($errors->any())
+        <div class="alert alert-danger">
+            <a href="#" class="close" data-dismiss="alert" aria-label="close">x</a>
+            <ul>
+                @foreach($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    @if (Session::has('success'))
+        <div class="alert alert-success">
+            <a href="#" class="close" data-dismiss="alert" aria-label="close">x</a>
+            <p>{{ Session::get('success') }}</p>
+        </div>
+    @endif        
+
     <div id="app">
         <div class="row">
             <div class="col-md-12">
